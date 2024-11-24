c3693f54f9c0a2a762d03d22fa31ef5f26ee9014
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-11-24 22:41:38 +0300

    design navbar

--- src/COMPONENTS/HEADER/Navbar.jsx
+++ src/COMPONENTS/HEADER/Navbar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import './Navbar.css'
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav style={{ padding: "10px 20px", backgroundColor: "#28a745", color: "#fff" }}>
+    <h1 style={{ display: "inline" }}>HealthConnect</h1>
+    <ul style={{ display: "inline", float: "right", listStyleType: "none", margin: 0 }}>
+      <li style={{ display: "inline", margin: "0 10px", cursor: "pointer" }}>Home</li>
+      <li style={{ display: "inline", margin: "0 10px", cursor: "pointer" }}>Doctors</li>
+      <li style={{ display: "inline", margin: "0 10px", cursor: "pointer" }}>Profile</li>
+    </ul>
+  </nav>
   )
 }
 
