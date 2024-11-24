d68c238e29f74ce0f542f916434b2971e87f2c8f
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-11-24 23:30:36 +0300

    implement part of apointment section

--- src/PAGES/HOME/Home.jsx
+++ src/PAGES/HOME/Home.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { useState } from 'react';
 import './Home.css'
 function Home() {
+  const SelectedDate = new Date().toISOString().split("T")[0];
+  const[ date,setDate] = useState(SelectedDate)
+ const[time,setTime] = useState(null)
+
+ function hadleReschedule(){
+
+ }
+  const appointments = [
+    { id: 1, doctorName: "Dr. Joseph Mbugua", date: <input type="date" defaultValue={SelectedDate}  disabled/>, time: "" },
+    { id: 1, doctorName: "Dr. Emmah Mbugua", date: <input type="date" defaultValue={SelectedDate}  disabled/>, time: "" },
+  ];
+
   return (
-    <div>Home</div>
+    <section className='home-component'>
+    <h2>Upcoming Appointments</h2>
+    <ul>
+      {appointments.map((currentAppointment) => (
+        <li
+          key={currentAppointment.id}>
+          <div>
+            <strong>{currentAppointment.doctorName}</strong>
+            <p>
+              {currentAppointment.date} at {currentAppointment.time}
+            </p>
+          </div>
+          <button>
+            Reschedule
+          </button>
+        </li>
+      ))}
+    </ul>
+  </section>
   )
 }
 
